43093c0ae898c4dd520df126f7c9ef0cbb889499
Himanshu Yadav <hy287719@gmail.com>
2024-09-17 09:26:44 +0530

    activity ui

--- src/pages/Activity/index.jsx
+++ src/pages/Activity/index.jsx
@@ -2,7 +2,6 @@ import { AvatarImage, Avatar } from "@/components/ui/avatar";
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
@@ -13,22 +12,27 @@ const Activity = () => {
     return (
         <div className="p-5 lg:px-20">
             <h1 className="pb-5 font-bold text-3xl">
-                Portfolio
+                Activity
             </h1>
-            <Table>
+            <Table className="border">
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Asset</TableHead>
-                        <TableHead>PRICE</TableHead>
-                        <TableHead>UNIT</TableHead>
-                        <TableHead>CHANGE</TableHead>
-                        <TableHead>CHANGE%</TableHead>
+                        <TableHead className="py-5">Date & Time</TableHead>
+                        <TableHead>Coin</TableHead>
+                        <TableHead>BUY PRICE</TableHead>
+                        <TableHead>SELL PRICE</TableHead>
+                        <TableHead>ORDER TYPE</TableHead>
+                        <TableHead>PROFIT/LOSS</TableHead>
                         <TableHead className="text-right">VALUE</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     {[...Array(5)].map(() => (
                         <TableRow>
+                            <TableCell className="text-left">
+                                <p> 2024/05/31 </p>
+                                <p className="text-gray-400"> 12:39:32 </p>
+                            </TableCell>
                             <TableCell className="font-medium flex items-center gap-3">
                                 <Avatar>
                                     <AvatarImage src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"></AvatarImage>
@@ -39,7 +43,7 @@ const Activity = () => {
                             <TableCell className="text-left"> 12345234</TableCell>
                             <TableCell className="text-left"> 123456788</TableCell>
                             <TableCell className="text-left"> 0.034% </TableCell>
-                            <TableCell className="text-right">$250 .00</TableCell>
+                            <TableCell>$250 .00</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
