3caae2eef519db9d1a111e44df94d40be4cbf80d
Himanshu Yadav <hy287719@gmail.com>
2024-09-21 22:11:40 +0530

    Added profile page

---
+++ src/pages/Profile/Enable2StepVerif.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+  } from "@/components/ui/input-otp"
+import { Button } from '@/components/ui/button';
+  
+
+
+const Enable2StepVerif = ({ handleSubmit }) => {
+    const [otp, setOTP] = useState("");
+    const handleSubmit1 = () => {
+        console.log("Submitted");
+    }
+    return (
+        <div className='flex justify-center'>
+            <div className='space-y-5 mt-10 w-full'>
+                <div className='flex justify-between items-center'>
+                    <p>Email :</p>
+                    <p>codewithzosh@gmail.com</p>
+                    <Dialog>
+                        <DialogTrigger>
+                            Sent OTP
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Enter OTP</DialogTitle>
+                            </DialogHeader>
+                            <div className='py-5 flex gap-10 justify-center items-center'>
+                                <InputOTP 
+                                    value={otp}
+                                    onChange={(value) => setOTP(value)}
+                                    maxLength={6}
+                                >
+                                    <InputOTPGroup>
+                                        <InputOTPSlot index={0} />
+                                        <InputOTPSlot index={1} />
+                                        <InputOTPSlot index={2} />
+                                    </InputOTPGroup>
+                                    <InputOTPSeparator />
+                                    <InputOTPGroup>
+                                        <InputOTPSlot index={3} />
+                                        <InputOTPSlot index={4} />
+                                        <InputOTPSlot index={5} />
+                                    </InputOTPGroup>
+                                </InputOTP>
+                                <DialogClose>
+                                    <Button 
+                                        onClick={() => {handleSubmit1()}}
+                                        className="w-[10rem]"
+                                    >Submit</Button>
+                                </DialogClose>
+                            </div>
+                        </DialogContent>
+                    </Dialog>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Enable2StepVerif
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,107 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { VerifiedIcon } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import Enable2StepVerif from "./Enable2StepVerif";
+  
+
 const Profile = () => {
+    const acitvate2StepVeri = () => {};
     return (
-        <div>
-            Profile
+        <div className="flex flex-col items-center mb-5">
+            <div className="pt-10 w-full lg:w-[60%]">
+                <Card>
+                    <CardHeader className="pb-9">
+                        <CardTitle>
+                            Your Information
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="lg:flex gap-32">
+                            <div className="space-y-7">
+                                <div className="flex">
+                                    <p className="w-[9rem]"> Email : </p>
+                                    <p className="text-gray-500"> example@mail.com </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> FullName : </p>
+                                    <p className="text-gray-500">Example Yadav </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> DOB : </p>
+                                    <p className="text-gray-500"> 18 March 1344 </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> Nationality : </p>
+                                    <p className="text-gray-500"> XYZ </p>
+                                </div>
+                            </div>
+                            <div className="space-y-7 lg:mt-0 mt-7">
+                                <div className="flex">
+                                    <p className="w-[9rem]"> Email : </p>
+                                    <p className="text-gray-500"> example@mail.com </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> FullName : </p>
+                                    <p className="text-gray-500">Example Yadav </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> DOB : </p>
+                                    <p className="text-gray-500"> 18 March 1344 </p>
+                                </div>
+                                <div className="flex">
+                                    <p className="w-[9rem]"> Nationality : </p>
+                                    <p className="text-gray-500"> XYZ </p>
+                                </div>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+
+                <div className="mt-6">
+                    <Card className="w-full ">
+                        <CardHeader className="pb-7">
+                            <div className="flex items-center gap-3">
+                                <CardTitle> 2 Step Verification</CardTitle>
+                                {false ? (
+                                    <Badge className="bg-orange-500 text-white"> Disabled  </Badge>
+                                ) : (
+                                    <Badge className="bg-green-500 space-x-2 text-white">
+                                        <VerifiedIcon />
+                                        <span>
+                                            Enabled
+                                        </span>
+                                    </Badge>
+                                )}
+                            </div>
+                        </CardHeader>
+                        <CardContent>
+                            <div>
+                                <Dialog>
+                                    <DialogTrigger>
+                                        <Button> Enable Two Step Verification</Button>
+                                    </DialogTrigger>
+                                    <DialogContent>
+                                        <DialogHeader>
+                                            <DialogTitle>Verify your account</DialogTitle>
+                                        </DialogHeader>
+                                        <Enable2StepVerif handleSubmit={acitvate2StepVeri} />
+                                    </DialogContent>
+                                </Dialog>
+
+
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     )
 };
